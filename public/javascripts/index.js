cf00431dfb9126506d1ca43a1deedc5f57e8d052
sayak119 <sk.sayakkundu1997@gmail.com>
2018-04-01 18:41:55 +0530

    added

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -112,6 +112,7 @@ document.addEventListener('DOMContentLoaded', () => {
       $("#menu").empty();
       $("#menu").append('<li>Tweets:</li>');
       res.tweets.map((item, index) => {
+        console.log(item);
         $("#menu").append('<li class="tweet-id">Tweet ID: '+item.id+'</li>');
         $("#menu").append('<li class="tweet-item">'+item.text+'</li>');
       });
@@ -173,22 +174,8 @@ document.addEventListener('DOMContentLoaded', () => {
           datasets: [{
               label: 'Tweet Tones',
               data: dat,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              backgroundColor: 'rgba(200,0,0,0.2)',
+              borderColor: 'rgba(200,0,0,1)',
               borderWidth: 1
           }]
         },

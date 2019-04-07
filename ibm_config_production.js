ca97918cca910f636312a79a0b8997d0fcc5393d
Sayak Kundu <sk.sayakkundu1997@gmail.com>
2019-04-07 09:28:42 +0530

    Update ibm_config

--- ibm_config_production.js
+++ ibm_config_production.js
@@ -1,6 +1,6 @@
 module.exports = {
   "url": 'https://gateway.watsonplatform.net/tone-analyzer/api',
-  "username": '491720b7-2761-4d39-85ff-95074c9d898c',
-  "password": 'mcD31ZKTGyDP',
+  "username": '',
+  "password": '',
   "version_date": "2016-05-19"
 };
